d96149eed43f38ad70521fbb4879ed93776f9026
White <douhuanzhang@gmail.com>
2019-04-12 19:05:26 +0800

    Revert "version1.0"
    This reverts commit 6d6bc90ed40852a2e70959400518bae1a755c3f5.

--- A£∫∆˚≥µ≈∆’’≤È—ØœµÕ≥/∆˚≥µ≈∆’’≤È—ØœµÕ≥‘¥¥˙¬Î/Choice.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHOICE_H_INCLUDED
-#define CHOICE_H_INCLUDED
-
-void Header_choose();
-void Line();
-void Space(int n);
-bool Search_Block(StaticLinkList S,int c,ElemType elem,char ch);
-
-#endif // CHOICE_H_INCLUDED
